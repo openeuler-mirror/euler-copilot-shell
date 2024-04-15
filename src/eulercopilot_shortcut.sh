ee4b92e8c518639e75b6ca38a65470889a2ef7c3
史鸿宇 <shywzt@iCloud.com>
2024-04-15 11:51:59 +0800

    修复快捷键
    Signed-off-by: 史鸿宇 <shywzt@iCloud.com>

--- src/eulercopilot_shortcut.sh
+++ src/eulercopilot_shortcut.sh
@@ -1,11 +1,13 @@
 run_copilot() {
+    local terminal_settings=$(stty -g)
     local readline="${READLINE_LINE}"
     if [[ -z "${readline}" ]]; then
         READLINE_LINE="copilot "
         READLINE_POINT=${#READLINE_LINE}
     elif [[ ! "${readline}" =~ ^copilot ]]; then
-        READLINE_LINE="copilot '${readline}'"
-        READLINE_POINT=${#READLINE_LINE}
+        READLINE_LINE=""
+        stty sane && (copilot "${readline}")
     fi
+    stty "${terminal_settings}"
 }
 bind -x '"\C-h": run_copilot'
